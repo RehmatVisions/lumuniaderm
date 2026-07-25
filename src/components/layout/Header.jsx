e2ff0b5980d6c89c64696f7b14e5a4988468104c
Nova <novaderm4@gmail.com>
2026-07-25 22:40:07 +0500

    unfix nv

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar"
 
 export default function Header() {
   return (
-    <header className="fixed left-0 right-0 top-0 z-50">
+    <header className="relative left-0 right-0 top-0 z-50">
       <TopBar />
       <Navbar />
     </header>
